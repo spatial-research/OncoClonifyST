b5852d7b7698dfeb3b8c54e4c676368f7777bb6d
mengxiao-he <mengxiao.he@scilifelab.se>
2026-06-02 15:06:14 +0200

    added globalVariables to globals.R

---
+++ R/globals.R
@@ -0,0 +1,9 @@
+utils::globalVariables(c(
+  ".", ":=", "barcode", "chr", "from", "to", "label",
+  "from_label", "to_label", "same_label_neighbor",
+  "n_same_label_neighbors", "cleaned_label", "section",
+  "island_size", "Section", "group", "value", "prop",
+  "ypos_center", "xstart", "xend", "midpoint", "x", "y",
+  "Gene", "Expression", "nFeature_Spatial", "nCount_Spatial",
+  "cluster", "cnv", "avg", "size", "cnv_norm"
+))
